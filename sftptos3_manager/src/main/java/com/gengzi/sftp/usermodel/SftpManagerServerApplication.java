d276952cd358e30d172b206c73124521925b8c78
耿子 <1164014750@qq.com>
2025-09-17 14:33:50 +0800

    feat(project): 新增 sftptos3 管理端模块
    - 新增 sftptos3_manager 模块，用于管理 SFTP 服务器
    - 实现用户认证、日志记录等功能- 添加数据库连接配置
    -移动 core 模块中的部分类到 manager 模块

---
+++ sftptos3_manager/src/main/java/com/gengzi/sftp/usermodel/SftpManagerServerApplication.java
@@ -0,0 +1,12 @@
+package com.gengzi.sftp.usermodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftpManagerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SftpManagerServerApplication.class, args);
+    }
+}
